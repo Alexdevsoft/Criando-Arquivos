52b1c538479c1a832ff2c84a30c9ecfb7f8a73db
Alexdevsoft <alexhavilla2022@gmail.com>
2022-12-25 16:22:20 -0300

    Criando arquivo

---
+++ src/application/CriandoAquivoTest.java
@@ -0,0 +1,26 @@
+package application;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class CriandoAquivoTest {
+
+	public static void main(String[] args) {
+		
+		String[] lines = new String[] {"Good morning", "Good afternoon", "Good night"};
+		
+		String path = "c:\\temp\\out.txt";
+		
+		try(BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(path, true))){
+			for(String line : lines) {
+				bufferedWriter.write(line);
+				bufferedWriter.newLine();
+			}
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
